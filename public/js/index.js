be4ce8627813e067d0935911a8402d3f722cd409
chubbymaus <court@chubbymaus.com>
2018-05-11 22:45:09 -0600

    Emit newMessage on create message

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@ const socket = io();
 socket.on('connect', function () {
   console.log('connected to server');
 
-  socket.emit('createMessage', {
-    from: 'Court',
-    text: 'hey this is Court'
-  })
 });
 
 socket.on('disconnect', function () {
