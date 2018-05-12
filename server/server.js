dabe645db8de3ba8ab39f57d76b8a3f55e26bbd2
chubbymaus <court@chubbymaus.com>
2018-05-11 21:12:37 -0600

    basic socket.io connect/disconnect handlers

--- server/server.js
+++ server/server.js
@@ -1,13 +1,26 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
+
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
+});
 
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`server is running on ${port}`);
 });
